45fbfaa70590117217975fa01cc1c25ec630f645
Kaue Almeida <kaue03ago@gmail.com>
2024-09-04 09:10:14 -0300

    teste Caminho

--- Xadrez/src/Main.java
+++ Xadrez/src/Main.java
@@ -7,5 +7,17 @@ public class Main {
         //TESTE TABULEIRO
         Tabuleiro t = new Tabuleiro();
         System.out.println(t.desenho());
+
+
+        // TESTE CAMINHO
+        //  "a2" ate "a4")
+        String sequencia = "2a3a4a";
+        Caminho caminho = new Caminho(t, sequencia);
+
+        // imprimindo
+        System.out.println("Teste Caminho:");
+        System.out.println("Casa Inicial: " + caminho.casaInicial());
+        System.out.println("Casa Final: " + caminho.casaFinal());
+        System.out.println("Caminho está livre? " + caminho.estaLivre());
     }
 }
