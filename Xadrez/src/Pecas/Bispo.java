6f27942daed7ad3263973b8b84c4916d9396cf5f
anneSS1 <anne.sakai@estudante.ufscar.br>
2024-09-06 16:06:51 -0300

    arrumando caminho bispo

--- Xadrez/src/Pecas/Bispo.java
+++ Xadrez/src/Pecas/Bispo.java
@@ -12,7 +12,7 @@ public class Bispo extends Peca {
     }
 
     @Override
-    public boolean movimentoValido(int linhaOrigem, int colunaOrigem, int linhaDestino, int colunaDestino) {
+    public boolean movimentoValido(int linhaOrigem, char colunaOrigem, int linhaDestino, char colunaDestino) {
         int diferencaLinha = Math.abs(linhaDestino - linhaOrigem);
         int diferencaColuna = Math.abs(colunaDestino - colunaOrigem);
 
@@ -21,14 +21,14 @@ public class Bispo extends Peca {
     }//verifica se a diferença de linha e igual a diferença de coluna. Se for, o movimento eh diagonal
 
     @Override
-    public String caminho(int linhaOrigem, int colunaOrigem, int linhaDestino, int colunaDestino) {
+    public String caminho(int linhaOrigem, char colunaOrigem, int linhaDestino, char colunaDestino) {
         if (movimentoValido(linhaOrigem, colunaOrigem, linhaDestino, colunaDestino)) {
             StringBuilder caminho = new StringBuilder();
             int incrementoLinha = Integer.compare(linhaDestino, linhaOrigem);
             int incrementoColuna = Integer.compare(colunaDestino, colunaOrigem);
 
             int linhaAtual = linhaOrigem;
-            int colunaAtual = colunaOrigem;
+            char colunaAtual = colunaOrigem;
 
             while (linhaAtual != linhaDestino && colunaAtual != colunaDestino) {
                 caminho.append(linhaAtual).append(colunaAtual);
